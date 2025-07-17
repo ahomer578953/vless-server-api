29c9ca04456e6632e65cee7399492f704c5a7102
ahomer578953 <ahomer578953@gmail.com>
2025-07-17 05:59:50 +0200

    index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
   res.send('VLESS Server API is running 🚀');
 });
 
+app.get('/generate', (req, res) => {
+  const injectorData = {
+    message: "Here is your injection data",
+    uuid: "abcdef12-3456-7890-abcd-ef1234567890",
+    flow: "xtls-rprx-vision",
+    additionalInfo: "You can customize this object"
+  };
+  res.json(injectorData);
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
